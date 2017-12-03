3cfab6859f6cd6b7ce5b4e76016da0235ff5179e
sgodoroja <stefan.godoroja@gmail.com>
2017-12-03 15:36:00 +0200

    Designed "FurnitureList" and "FurnitureDetails" views.

--- Shinyture/ViewControllers/FurnitureListViewController.swift
+++ Shinyture/ViewControllers/FurnitureListViewController.swift
@@ -28,28 +28,70 @@
 
 import UIKit
 
+extension UIView {
+  
+  func setupShadow() {
+    self.layer.cornerRadius = 8
+    self.layer.shadowOffset = CGSize.zero
+    self.layer.shadowOpacity = 0.3
+    self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds,
+                                         byRoundingCorners: .allCorners,
+                                         cornerRadii: CGSize(width: 8, height: 8)).cgPath
+    self.layer.shouldRasterize = true
+    self.layer.rasterizationScale = UIScreen.main.scale
+  }
+  
+}
+
+class FurnitureOverviewView: UIView {
+
+}
+
+class FurnitureImageView: UIImageView {
+  
+  override var bounds: CGRect {
+    didSet {
+      setupShadow()
+    }
+  }
+}
+
 class FurnitureListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.title = "Shinyture"
+    let titles = ["Today", "Best Deals", "Categories"]
+    let segmentedControl = TwicketSegmentedControl(frame: CGRect.zero)
+    segmentedControl.setSegmentItems(titles)
+    view.addSubview(segmentedControl)
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    segmentedControl.snp.remakeConstraints { (maker) in
+      maker.height.equalTo(40)
+      maker.leadingMargin.equalTo(view.snp.leadingMargin).offset(5)
+      maker.topMargin.equalTo(view.snp.topMargin).offset(10)
+      maker.trailingMargin.equalTo(view.snp.trailingMargin).offset(5)
     }
-    */
+  }
+}
 
+extension FurnitureListViewController: UITableViewDelegate, UITableViewDataSource {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    performSegue(withIdentifier: "show.furnituredetails.segue", sender: nil)
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 6;
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "rw.furnitureoverview.cell")
+    return cell!
+  }
+  
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 300
+  }
+  
 }
